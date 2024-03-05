446ea2c1a142efab4bbcdfb0f1aa4a8833c9af36
Ogulcan <account.work@ogulcan.dev>
2024-03-05 15:53:43 +0300

    Update page.tsx

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
     <main className="">
-      <section
-        id="hero"
-        className="font-regular space-y-4 text-lg text-slate-100"
-      >
+      <section id="hero" className="font-regular space-y-4 text-lg text-slate-100">
         <p className="">Manifesto first paragraph.</p>
         <p className="font-regular text-md text-">
           Manifesto second paragraph. It is huge problem description.
